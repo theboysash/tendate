3c088288970f1967e0a6e3e7f588f0e1381a385e
theboysash <2733820@students.wits.ac.za>
2026-06-20 15:06:45 +0200

    More practicing

--- test/practice_api.py
+++ test/practice_api.py
@@ -2,13 +2,21 @@ from fastapi import FastAPI
 import sqlite3
 from data import books
 from pydantic import BaseModel
+from typing import Optional
+
 
 class Book(BaseModel):
     id: int
     title: str
     author: str
+    year: int
 
 app = FastAPI()
+
+class BookUpdate(BaseModel):
+    title: Optional[str] = None
+    author: Optional[str] = None
+    year: Optional[int] = None
 
 @app.get("/books")
 def get_books():
@@ -33,4 +41,37 @@ def create_book(book: Book):
     books.append(book)
     return book
 
+@app.delete("/books/{id}")
+def delete_book(id: int):
+    b = next((book for book in books if book["id"]==id), None)
+    if not b:
+        return {"error": "not found"}
+    books.remove(b)
+    return {"message": "deleted"}
+
+@app.put("/books/{id}")
+def update_details(id: int, book: Book):
+    b = next((bo for bo in books if bo["id"]==id), None)
+    if not b:
+        return {"message": "not found"}
+    b["year"] = book.year
+    b["title"] = book.title
+    b["author"] = book.author
+    b["id"] = book.id
+    return b
+
+    
+@app.patch("/books/{id}")
+def update_detail(id: int, book: BookUpdate):
+    b = next((bo for bo in books if bo["id"]==id), None)
+    if not b:
+        return {"message": "not found"}
+    if book.title is not None:
+        b["title"] = book.title
+    if book.author is not None:
+        b["author"] = book.author
+    if book.year is not None:
+        b["year"] = book.year
+    return b
+   
 
